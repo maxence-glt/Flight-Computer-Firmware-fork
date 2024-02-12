ffb89869a8b03edf2dc67abd5299a811c843f19f
jakenathanson15 <157774694+jakenathanson15@users.noreply.github.com>
2024-02-12 18:34:03 -0500

    Update LTC2990Driver2.cpp

--- src/drivers/LTC2990Driver2.cpp
+++ src/drivers/LTC2990Driver2.cpp
@@ -1,3 +1,8 @@
 
-updated
-helloo
+// we want to isolate parts of the driver (see discord) that are relevant to the functions of our LTC 2990.
+
+// Board purposes: 
+
+// Reduntant parts of our template driver:
+
+// usable parts of our template driver:
