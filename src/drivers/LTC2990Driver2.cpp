039faaa260a916f28c42d32aa8b9cdccd01c37a9
JmyaEngineering <jdmyer@buffalo.edu>
2024-02-19 18:48:17 -0500

    Commit 14 bit adc read

--- src/drivers/LTC2990Driver2.cpp
+++ src/drivers/LTC2990Driver2.cpp
@@ -36,6 +36,7 @@ int8_t LTC2990_adc_read(uint8_t i2c_address, uint8_t msb_register_address, int16
 // Similar to LTC2990_adc_read except it repeats until the data_valid bit is set, it fails to receive an I2C acknowledge, or the timeout (in milliseconds)
 // expires. It keeps trying to read from the LTC2990 every millisecond until the data_valid bit is set (indicating new data since the previous
 // time this register was read) or until it fails to receive an I2C acknowledge (indicating an error on the I2C bus).
+
 int8_t LTC2990_adc_read_timeout(uint8_t i2c_address, uint8_t msb_register_address, int16_t *adc_code, int8_t *data_valid, uint16_t timeout, uint8_t status_bit)
 {
     int8_t ack = 0;
@@ -61,6 +62,39 @@ int8_t LTC2990_adc_read_timeout(uint8_t i2c_address, uint8_t msb_register_addres
     }
     return(ack);
 }
+
+// Reads new data (even after a mode change) by flushing old data and waiting for the data_valid bit to be set.
+// This function simplifies adc reads when modes are changing.  For example, if V1-V2 changes from temperature mode
+// to differential voltage mode, the data in the register may still correspond to the temperature reading immediately
+// after the mode change.  Flushing one reading and waiting for a new reading guarantees fresh data is received.
+// If the timeout is reached without valid data (*data_valid=1) the function exits.
+
+int8_t LTC2990_adc_read_new_data(uint8_t i2c_address, uint8_t msb_register_address, int16_t *adc_code, int8_t *data_valid, uint16_t timeout)
+
+{
+    int8_t ack = 0;
+    uint8_t status_bit;
+    status_bit  = msb_register_address/2-1;
+    ack |= LTC2990_adc_read_timeout(i2c_address, msb_register_address, adc_code, data_valid, timeout, status_bit); //! 1)  Throw away old data
+
+    ack |= LTC2990_adc_read_timeout(i2c_address, msb_register_address, adc_code, data_valid, timeout,status_bit); //! 2) Read new data
+
+    return(ack);
+}
+
+// Reads an 8-bit register from the LTC2990 using the standard repeated start format.
+
+int8_t LTC2990_register_read(uint8_t i2c_address, uint8_t register_address, uint8_t *register_data)
+{
+    int8_t ack = 0;
+
+    ack = i2c_read_byte_data(i2c_address, register_address, register_data);
+    return(ack);
+}
+
+
+
+
 
 
 
